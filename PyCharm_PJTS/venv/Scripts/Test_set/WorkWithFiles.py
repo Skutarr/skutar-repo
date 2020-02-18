7ddfbeb1057d5e3feeb2a07d096482e3e45556ec
Skutarr <rom65904411@yandex.ru>
2020-02-18 21:10:34 +0300

    NewCommit_Add_Work_With_Files

---
+++ PyCharm_PJTS/venv/Scripts/Test_set/WorkWithFiles.py
@@ -0,0 +1,12 @@
+inputfile= "ex.txt"
+
+# открытие файла
+inputfile = 'ex.txt'
+myfile1 = open(inputfile, mode = 'r', encoding = "latin_1")
+myfile2 = open(outputfile, mode = 'w', encoding = "latin_1")
+
+myfile2.write()
+
+myfile1 = close()
+myfile2 = close()
+
