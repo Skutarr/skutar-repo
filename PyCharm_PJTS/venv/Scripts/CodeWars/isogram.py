cc6416b56203c670d9dc97901d6bc7d91255d181
Skutarr <rom65904411@yandex.ru>
2020-02-27 20:39:04 +0300

    Add sorting algo

--- PyCharm_PJTS/venv/Scripts/CodeWars/isogram.py
+++ PyCharm_PJTS/venv/Scripts/CodeWars/isogram.py
@@ -1,10 +1,12 @@
-def fun():
-    
+import re
+
+def fun(x):
 
 
-
-var=[True, True, True, False,
-     True, False, True, False]
-
-print(fun(var))
-
+var_str=("1 2 3 4 5")
+с={1,2,3,4,5}
+print(fun(var_str))
+# return "5 1"
+# high_and_low("1 2 -3 4 5") # return "5 -3"
+# high_and_low("1 9 3 4 -5") # return "9 -5"
+# print(var_str[2])
